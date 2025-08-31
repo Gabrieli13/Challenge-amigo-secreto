304b1af0b401b7106bbcf9c4d1b148919873c59a
Gabrieli Rodrigues <gabriele.rodrigues.rcf@gmail.com>
2025-08-31 18:27:56 -0300

    adicionando nomes na lista,sorteando,e mudança de cores

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeNomes = [];
+
+function adicionarAmigo() {
+    let nome = document.getElementById("amigo").value;
+    if (nome == "" ){
+         alert("Campo vazio, digite um nome");   
+    } else {
+       listaDeNomes.push(nome);
+       document.getElementById("listaAmigos").innerHTML = listaDeNomes.map(n => `<li>${n}</li>`).join("");
+       document.getElementById("amigo").value = "";
+    }
+};
+
+function sortearAmigo() {
+    if (listaDeNomes.length > 0) {
+        let indice = Math.floor(Math.random() * listaDeNomes.at.length);
+        document.getElementById("resultado").innerHTML = `<li>Sorteado: ${listaDeNomes[indice]}</li>`;
+    }
+};
